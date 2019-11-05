59f9a7a948cde00f301e650076c3493eccdf949f
Cristiano1782 <cristiano.panizza@marconirovereto.it>
2019-11-05 14:34:50 +0100

    3 files modified

--- src/app/contatto.component/contatto.component.ts
+++ src/app/contatto.component/contatto.component.ts
@@ -10,4 +10,8 @@ import { Contatto } from './contatto.model';
 export class ContattoComponent{
   contatto: Contatto = new Contatto('Cristiano', 'Panizza', 'panizzacristiano@gmail.com', '123456789', 5);
 
+setTextColor(){
+  return this.contatto.isPreferito()? "red": "black";
+}
+
 }
